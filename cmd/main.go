572bc7a533fc98fff9fd7bd674af9e4b9205221c
Sam Bradshaw <bradshaw.sa@yahoo.com>
2025-08-30 13:36:41 +0100

    Implemented fetch email

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/emersion/go-imap"
+	"github.com/jhillyerd/enmime"
 	"github.com/samothreesixty/EmailScraper/internal/config"
-	"github.com/samothreesixty/EmailScraper/internal/imap"
-	// "github.com/emersion/go-imap"
+	"github.com/samothreesixty/EmailScraper/internal/db"
+	"github.com/samothreesixty/EmailScraper/internal/imap_init"
 )
 
 func main() {
@@ -15,15 +19,20 @@ func main() {
 		log.Fatal(err)
 	}
 
+	dbConn, err := config.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Connecting to server...")
-	c, err := imap.Connect(conf.Host, conf.Port, conf.Username, conf.Password)
+	c, err := imap_init.Connect(conf.Host, conf.Port, conf.Username, conf.Password)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer c.Logout()
 	fmt.Println("Connected and logged in!")
 
-	messages, err := imap.FetchLastUnseen(c, conf.Inbox, 4)
+	messages, err := imap_init.FetchLastUnseen(c, conf.Inbox, 4)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,8 +42,43 @@ func main() {
 		return
 	}
 
+	section := &imap.BodySectionName{}
+
 	fmt.Println("Last unseen messages:")
 	for _, msg := range messages {
-		fmt.Println("* " + msg.Envelope.Subject)
+		var body string
+		r := msg.GetBody(section)
+		if r != nil {
+			env, err := enmime.ReadEnvelope(r)
+			if err != nil {
+				log.Println("Failed to parse MIME:", err)
+			} else {
+				body = env.HTML
+			}
+		}
+
+		err := dbConn.InsertEmail(context.Background(), db.InsertEmailParams{
+			Subject:   msg.Envelope.Subject,
+			FromEmail: formatAddressList(msg.Envelope.From),
+			ToEmail:   formatAddressList(msg.Envelope.To),
+			DateSent:  msg.Envelope.Date,
+			Body:      body,
+			CreatedAt: time.Now(),
+		})
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 	}
+}
+
+func formatAddressList(list []*imap.Address) string {
+	var result string
+	for i, addr := range list {
+		if i > 0 {
+			result += ", "
+		}
+		result += addr.MailboxName + "@" + addr.HostName
+	}
+	return result
 }
